399c40e85cedbcb601af511d8180908655cfacb3
Lorrayne <lorrayne.traspadini@gmail.com>
2023-08-30 16:08:51 -0300

    ATT COND S, C E ENC + ADD SWITCH CASE

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -1,14 +1,17 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int Nota = 9;
-
+        int Nota = 5;
+        /**CONDICIONAL ENCADEADA */
         if (Nota >= 7)
         System.out.println("Aprovado");
         else if (Nota >= 5 && Nota < 7 )
         System.out.println("Recuperação");
         else
         System.out.println("Reprovado");
+
+    /**CONDIÇÃO TERNARIA */
+    String resultado = Nota >= 7 ? "Aprovado" : Nota >= 5 && Nota < 7 ? "Recuperação" : "Reprovado"; //ABREVIAÇÃO DO IF/ELSE
+    //SINTAXE = [CONDIÇÃO 1 ] ? [SE COOND VDD] : [SE COND FALSA][AQUI VOCE PODE INSERIR OUTRA COND 2 TBM, CASO FAÇA USAR ?] : [RESULT DA COND 2];
+    System.out.println(resultado);
     }
 }
-
-/** EXEMPLO DE CONDICIONAL ENCADEADA */
