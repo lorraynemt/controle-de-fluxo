8e2fc2bd8a101a9e447e5a4ada3d62143b54ec92
Lorrayne <lorrayne.traspadini@gmail.com>
2023-08-30 14:14:48 -0300

    first commit

---
+++ src/CaixaEletronico.java
@@ -0,0 +1,16 @@
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        double saldo = 25.0;
+        double valorSolicitado = 27.0;
+
+        System.out.println("Saldo: " + saldo);
+
+        if(valorSolicitado < saldo)
+            saldo = saldo - valorSolicitado;
+        
+        else
+            System.out.println("Saldo insuficiente.");
+
+        System.out.println("Novo saldo: " + saldo);
+    }
+}
